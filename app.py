b7200c1f88d88b761e82984bbd2cd32ba626e58c
junhan zhang <junhanzhang@Mac.attlocal.net>
2025-04-30 23:31:03 -0700

    second query

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from google.cloud import bigquery
 app = Flask(__name__)
 client = bigquery.Client()
 
-# 你的 Query，注意要用 @state 和 @county 作为参数
 QUERY = """
 WITH
   crashes AS (
@@ -22,48 +21,14 @@ WITH
     ON 
       v.ST_CASE = a.ST_CASE AND v.L_YEAR = a.YEAR
     WHERE 
-      a.STATE = @state -- 或者直接去掉STATE限制，算全美国
+      a.STATE = @state
     GROUP BY 
       a.COUNTY
   ),
-  scores AS(
+
+  score1 AS(
   SELECT
     COUNTY,
-    
-    pedestrian_count,
-    fire_count,
-    motor_count,
-    rollover_count,
-    (total_count - pedestrian_count - fire_count - motor_count - rollover_count) AS other_count,
-
-    -- 各事故类型比例
-    SAFE_DIVIDE(pedestrian_count, total_count) AS pedestrian_rate,
-    SAFE_DIVIDE(fire_count, total_count) AS fire_rate,
-    SAFE_DIVIDE(motor_count, total_count) AS motor_rate,
-    SAFE_DIVIDE(rollover_count, total_count) AS rollover_rate,
-    SAFE_DIVIDE(total_count - pedestrian_count - fire_count - motor_count - rollover_count, total_count) AS other_rate,
-
-    -- 加权得分
-    (
-      SAFE_DIVIDE(pedestrian_count, total_count) * 1.7 +
-      SAFE_DIVIDE(fire_count, total_count) * 1.8 +
-      SAFE_DIVIDE(motor_count, total_count) * 1.2 +
-      SAFE_DIVIDE(rollover_count, total_count) * 1.5 +
-      SAFE_DIVIDE(total_count - pedestrian_count - fire_count - motor_count - rollover_count, total_count) * 1.0
-    ) AS weighted_score,
-
-    -- Raw Risk Score
-    (
-      (
-        SAFE_DIVIDE(pedestrian_count, total_count) * 1.7 +
-        SAFE_DIVIDE(fire_count, total_count) * 1.8 +
-        SAFE_DIVIDE(motor_count, total_count) * 1.2 +
-        SAFE_DIVIDE(rollover_count, total_count) * 1.5 +
-        SAFE_DIVIDE(total_count - pedestrian_count - fire_count - motor_count - rollover_count, total_count) * 1.0
-      ) * LOG(total_count + 1)--* total_count
-    ) AS raw_risk_score,
-
-    -- 归一化成0-100
     (
       ( 
         (
@@ -82,14 +47,48 @@ WITH
           SAFE_DIVIDE(total_count - pedestrian_count - fire_count - motor_count - rollover_count, total_count) * 1.0
         ) * LOG(total_count + 1) --* total_count
       ) OVER ()
-    ) * 100 AS region_risk_score
+    ) * 100 AS risk_score
 
   FROM crashes
-  --ORDER BY region_risk_score DESC
+  --ORDER BY risk_score DESC
 )
-SELECT *
-FROM scores
-WHERE county=@county
+
+
+,
+bad_rate AS(
+  SELECT 
+    COUNTY,
+    COUNTIF(
+      WEATHER IN (
+        'Freezing Rain or Drizzle',
+        'Blowing Snow',
+        'Rain',
+        'Snow',
+        'Fog, Smog, Smoke',
+        'Severe Crosswinds',
+        'Blowing Sand, Soil, Dirt'
+      )
+    )/COUNT(*) AS bad_Weather_rate
+  FROM 
+    `bigquery-public-data.dataflix_traffic_safety.accident`
+  WHERE 
+    STATE = @state
+  GROUP BY 
+    COUNTY
+  ORDER BY bad_Weather_rate DESC
+),
+score2 AS(
+  SELECT COUNTY, bad_Weather_rate,(bad_Weather_rate/MAX(bad_Weather_rate) OVER())*100 AS risk_score
+  FROM bad_rate
+  ORDER BY risk_score DESC
+)
+
+
+SELECT s1.COUNTY, (s1.risk_score+s2.risk_score)/2 AS region_risk_score
+FROM score1 s1
+JOIN score2 s2
+on s1.COUNTY=s2.COUNTY
+WHERE s1.COUNTY=@county
 """
 @app.route('/')
 def home():
