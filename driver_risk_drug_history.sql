6aa60e3811dcbe3fd6c7289cb636b49a65f2e837
Parth-Panchal-284 <pcpanchal284@gmail.com>
2025-05-01 22:06:18 -0700

    Optimized/Unoptimized swap: Drug history

--- driver_risk_drug_history.sql
+++ driver_risk_drug_history.sql
@@ -1,4 +1,21 @@
-WITH drug_fatalities AS (
+SELECT 
+  DRUGRES,
+  total_fatalities / (SELECT SUM(total_fatalities) FROM (
+    SELECT 
+      d.DRUGRES, 
+      SUM(a.FATALS) AS total_fatalities
+    FROM 
+      `bigquery-public-data.dataflix_traffic_safety.person` p,
+      `bigquery-public-data.dataflix_traffic_safety.drugs` d,
+      `bigquery-public-data.dataflix_traffic_safety.accident` a
+    WHERE 
+      p.L_YEAR = d.L_YEAR AND 
+      p.ST_CASE = d.ST_CASE AND 
+      p.L_YEAR = a.L_YEAR AND 
+      p.ST_CASE = a.ST_CASE
+    GROUP BY d.DRUGRES
+  )) AS fatality_ratio
+FROM (
   SELECT 
     d.DRUGRES, 
     SUM(a.FATALS) AS total_fatalities
@@ -7,8 +24,8 @@ WITH drug_fatalities AS (
     ON p.L_YEAR = d.L_YEAR AND p.ST_CASE = d.ST_CASE 
   JOIN `bigquery-public-data.dataflix_traffic_safety.accident` a
     ON p.L_YEAR = a.L_YEAR AND p.ST_CASE = a.ST_CASE 
-  WHERE p.PER_TYP = "Driver of a Motor Vehicle in Transport" AND p.STATE = "California" -- @state
-    AND d.DRUGRES IN (
+  WHERE 
+    d.DRUGRES IN (
       "Depressant", 
       "Other Drug", 
       "Stimulant", 
@@ -22,16 +39,7 @@ WITH drug_fatalities AS (
       "Anabolic Steroid", 
       "Tested for Drugs, Results Unknown"
     )
+    AND p.PER_TYP = "Driver of a Motor Vehicle in Transport" AND p.STATE = "California" -- @state
   GROUP BY d.DRUGRES
 )
-
-SELECT
-  fatality_ratio 
-FROM (
-  SELECT
-    DRUGRES,
-    ROUND(total_fatalities / SUM(total_fatalities) OVER (), 4) AS fatality_ratio
-  FROM drug_fatalities
-)
-WHERE DRUGRES = "Other Drug" -- @drug
-;
+WHERE DRUGRES = "Other Drug" --@drug;
--- driver_risk_drug_optimized.sql
+++ driver_risk_drug_optimized.sql
@@ -1,34 +1,37 @@
+WITH drug_fatalities AS (
+  SELECT 
+    d.DRUGRES, 
+    SUM(a.FATALS) AS total_fatalities
+  FROM `bigquery-public-data.dataflix_traffic_safety.person` p 
+  JOIN `bigquery-public-data.dataflix_traffic_safety.drugs` d 
+    ON p.L_YEAR = d.L_YEAR AND p.ST_CASE = d.ST_CASE 
+  JOIN `bigquery-public-data.dataflix_traffic_safety.accident` a
+    ON p.L_YEAR = a.L_YEAR AND p.ST_CASE = a.ST_CASE 
+  WHERE p.PER_TYP = "Driver of a Motor Vehicle in Transport" AND p.STATE = "California" -- @state
+    AND d.DRUGRES IN (
+      "Depressant", 
+      "Other Drug", 
+      "Stimulant", 
+      "Cannabinoid", 
+      "Tested for Drugs, Drugs Found, Type Unknown/Positive", 
+      "Reported as Unknown If Tested for Drugs", 
+      "Narcotic", 
+      "Phencyclidine (PCP)", 
+      "Hallucinogen", 
+      "Inhalant", 
+      "Anabolic Steroid", 
+      "Tested for Drugs, Results Unknown"
+    )
+  GROUP BY d.DRUGRES
+)
+
 SELECT
-  DRUGRES,
-  total_fatalities,
-  ROUND(total_fatalities / (
-    SELECT SUM(a.FATALS)
-    FROM `bigquery-public-data.dataflix_traffic_safety.person` p
-    JOIN `bigquery-public-data.dataflix_traffic_safety.drugs` d
-      ON p.L_YEAR = d.L_YEAR AND p.ST_CASE = d.ST_CASE
-    JOIN `bigquery-public-data.dataflix_traffic_safety.accident` a
-      ON p.L_YEAR = a.L_YEAR AND p.ST_CASE = a.ST_CASE
-    WHERE p.PER_TYP = "Driver of a Motor Vehicle in Transport"
-      AND p.STATE = 'California' --@state
-      AND d.DRUGRES IN (
-        "Depressant", "Other Drug", "Stimulant", "Cannabinoid", 
-        "Tested for Drugs, Drugs Found, Type Unknown/Positive", 
-        "Reported as Unknown If Tested for Drugs", "Narcotic", 
-        "Phencyclidine (PCP)", "Hallucinogen", "Inhalant", 
-        "Anabolic Steroid", "Tested for Drugs, Results Unknown"
-      )
-  ), 4) AS fatality_ratio
+  fatality_ratio 
 FROM (
   SELECT
-    d.DRUGRES,
-    SUM(a.FATALS) AS total_fatalities
-  FROM `bigquery-public-data.dataflix_traffic_safety.person` p
-  JOIN `bigquery-public-data.dataflix_traffic_safety.drugs` d
-    ON p.L_YEAR = d.L_YEAR AND p.ST_CASE = d.ST_CASE
-  JOIN `bigquery-public-data.dataflix_traffic_safety.accident` a
-    ON p.L_YEAR = a.L_YEAR AND p.ST_CASE = a.ST_CASE
-  WHERE p.PER_TYP = "Driver of a Motor Vehicle in Transport"
-    AND p.STATE = 'California' --@state
-    AND d.DRUGRES = "Other Drug" -- @drug
-  GROUP BY d.DRUGRES
-) AS drug_data;
+    DRUGRES,
+    ROUND(total_fatalities / SUM(total_fatalities) OVER (), 4) AS fatality_ratio
+  FROM drug_fatalities
+)
+WHERE DRUGRES = "Other Drug" -- @drug
+;
